a5a9855bdfdc85360f3cca577ee94c46fc2f6d3f
Nate Kaiser <natejkaiser@gmail.com>
2017-06-02 01:55:30 -0500

    forgot to add this shell file, need to fill it in and feed data to interactive_plot script

---
+++ code/postprocess.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+''' script for post-processing output text from high-dimensionality experiment loop '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def postprocess():
+    pass
+
+
+if __name__ == '__main__':
+    postprocess()
