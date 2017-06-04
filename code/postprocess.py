2b69b7f5e67ff5558bbb1e08cba88f7975b3d67c
Adam Pollack <apollack11@gmail.com>
2017-06-04 15:53:41 -0500

    Adding graphs for new architectures

--- code/postprocess.py
+++ code/postprocess.py
@@ -9,7 +9,7 @@ from operator import itemgetter
 def postprocess():
     data = {}
     config = []
-    with open("data/experiment_output_FINAL.txt", 'r') as f:
+    with open("data/new_architectures.txt", 'r') as f:
         for line in f:
             line = line.strip().split()
             if line:
@@ -19,16 +19,17 @@ def postprocess():
                     count = 0
                     config = []
 
-            if count == 5 or count == 6:
-                config.append([int(line[2][1]), int(line[3][0])])
-            elif count == 7:
-                config.append(int(line[2]))
-            elif count == 8:
-                config.append(int(line[2]))
-            elif count == 9:
-                config.append(float(line[2]))
+            # UNCOMMENT FOR 108 DATA
+            # if count == 5 or count == 6:
+            #     config.append([int(line[2][1]), int(line[3][0])])
+            # elif count == 7:
+            #     config.append(int(line[2]))
+            # elif count == 8:
+            #     config.append(int(line[2]))
+            # elif count == 9:
+            #     config.append(float(line[2]))
                 # print "config:", config
-            elif count == 12:
+            if count == 12:
                 step = [int(x) for x in line[0].split(',')]
                 # print "step:", step
             elif count == 14:
@@ -56,16 +57,16 @@ def postprocess():
     averages = []
     for n in data:
         # print n
-        avg = sum(data[n]['validation_accuracy'][17:30])/(30.0-17.0)
+        avg = sum(data[n]['validation_accuracy'][21:30])/(30.0-21.0)
         averages.append([avg, n])
     averages.sort(key=lambda x: x[0])
-    # for a in averages:
-    #     print a
-
     averages = list(reversed(averages))
 
+    for a in averages:
+        print a
+
     fig, ax = plt.subplots()
-    for model in averages[0:5]:
+    for model in averages[0:6]:
         ax.plot(data[model[1]]['step'], data[model[1]]['validation_accuracy'], lw=1.0, label=('model ' + str(model[1])))
     ax.set_title('Validation Accuracy vs Epoch')
     ax.set_xlim([0, 500])
@@ -76,9 +77,9 @@ def postprocess():
     plt.show()
 
     fig, ax = plt.subplots()
-    for model in averages[0:5]:
+    for model in averages[0:6]:
         ax.plot(data[model[1]]['step'], data[model[1]]['loss'], lw=1.0, label=('model ' + str(model[1])))
-    ax.set_title('Validation Accuracy vs Epoch')
+    ax.set_title('Loss vs Epoch')
     ax.set_xlim([0, 500])
     ax.set_ylim([0, 0.5])
     ax.legend(loc='upper right')
